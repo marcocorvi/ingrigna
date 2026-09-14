10d819431407d93c8e88474f429381dcb71308b7
marcocorvi <marco.corvi@gmail.com>
2026-09-14 20:08:17 +0200

    population step: reports, schede, biblio-xml, some docs

---
+++ biblio/docs/SAVE/articoli.sql
@@ -0,0 +1,34 @@
+-- phpMyAdmin SQL Dump
+-- version 2.9.1
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generato il: 01 Ago, 2008 at 10:49 AM
+-- Versione MySQL: 5.0.51
+-- Versione PHP: 4.4.8
+-- 
+-- Database: `my_ingrigna`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Struttura della tabella `articoli`
+-- 
+
+-- 
+-- Dump dei dati per la tabella `articoli`
+-- 
+
+INSERT INTO `articoli` VALUES
+(254, 2004, 'Turri S., Citterio M., Bini A., Maggi V.', 'Lo Stefan Problem utilizzato per interpretare le condizioni di congelamento di un lago ipogeo nella ghiacciaia Lo Lc 1650 (Moncodeno, Lecco - Italia)', 'Le Grotte d''Italia, s. V, 5 (2004) 29-34', 3, '"0"', NULL);
+INSERT INTO `articoli` VALUES
+(255, 1996, 'aa. vv.', 'Capitano Paff (acte 2)', 'Aitsa, 3 1996, 8-11', 2, '"0"', NULL);
+INSERT INTO `articoli` VALUES
+(256, 1987, 'Buzio A.', '90 anni di speleologia', 'Il Grottesco, 48, 1987, 3-7', 2, '"0"', NULL);
+INSERT INTO `articoli` VALUES
+(257, 2004, 'Bini A., Pellegrini A., Zuccoli L.', 'Il carsismo in Moncodeno', 'Le Grotte d''Italia, s. V, 5 (2004) 1-28', 2, '"0"', NULL);
+INSERT INTO `articoli` VALUES
+(258, 1899, 'Cermenati M.', 'La ghiacciaia del Moncodeno', 'Rivista Mensile del CAI, v. XVIII, n. 2, feb. 1899, 3-14', 2, '"0"', NULL);
+INSERT INTO `articoli` VALUES
+(259, 1922, 'Guzzi C.', 'Alle origini del Fiumelatte', 'Le Vie d''Italia, 10, 1922, 1007-1009', 2, '"0"', NULL);
